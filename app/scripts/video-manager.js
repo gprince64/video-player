dd61443c23ecea08d831d372a5a32f84e41e5948
gprince64 <gregory.prince64@gmail.com>
2017-08-30 19:00:36 +0200

    Implementing optionnal functionnalities

--- app/scripts/video-manager.js
+++ app/scripts/video-manager.js
@@ -1,6 +1,7 @@
 // Initializing elements
 var videoPlayer;
 var playPauseBtn;
+var muteBtn
 var progressBar;
 
 
@@ -11,6 +12,8 @@ function initVideoPlayer(){
 	videoPlayer.controls = false;
 
 	playPauseBtn = document.getElementById('play-pause-button');
+
+	muteBtn = document.getElementById('mute-button');
 
 	progressBar = document.getElementById('progress-bar');
 	progressBar.addEventListener('click', clickedBar, false);
@@ -23,9 +26,24 @@ function initVideoPlayer(){
 		changeButton(playPauseBtn, 'play');
 	}, false);
 
+	videoPlayer.addEventListener('volumechange', function(){
+		if(videoPlayer.muted)
+			changeButton(muteBtn,'unmute');
+		else
+			changeButton(muteBtn,'mute');
+	}, false);
+
 	videoPlayer.addEventListener('timeupdate', updateProgressBar, false);
+	
 	videoPlayer.addEventListener('ended', resetVideo, false);
 
+}
+
+function replayVideo(){
+	videoPlayer.currentTime = 0;
+	progressBar.value = 0;
+	changeButton(playPauseBtn,'play');
+	videoPlayer.play();
 }
 
 function togglePlayPause(){
@@ -38,18 +56,41 @@ function togglePlayPause(){
 	}
 }
 
-function updateProgressBar(){
-	var currentTimePercentage = Math.floor((100/videoPlayer.duration) * videoPlayer.currentTime);
-	progressBar.value = currentTimePercentage;
-	progressBar.innerHTML = currentTimePercentage + '%';
-}
-
 function resetVideo(){
 	videoPlayer.currentTime = 0;
 	progressBar.value = 0
 	changeButton(playPauseBtn, 'play');
 	videoPlayer.pause();
 }
+
+function volumePlus(){
+	if(videoPlayer.volume < 1)
+		videoPlayer.volume += 0.1;
+}
+
+function volumeLess(){
+	if(videoPlayer.volume > 0)
+		videoPlayer.volume -= 0.1;
+}
+
+function toggleMute(){
+	if(videoPlayer.muted){
+		changeButton(muteBtn,'mute');
+		videoPlayer.muted = false;
+	}
+	else{
+		changeButton(muteBtn,'unmute');
+		videoPlayer.muted=true;
+	}
+}
+
+
+function updateProgressBar(){
+	var currentTimePercentage = Math.floor((100/videoPlayer.duration) * videoPlayer.currentTime);
+	progressBar.value = currentTimePercentage;
+	progressBar.innerHTML = currentTimePercentage + '%';
+}
+
 
 function clickedBar(e){
 	var time = (e.pageX  - (this.offsetLeft + this.offsetParent.offsetLeft)) / this.offsetWidth;

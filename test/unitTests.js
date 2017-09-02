4bb4a93eed4578754e109ff351a877a4d041f1b5
gprince64 <gregory.prince64@gmail.com>
2017-09-02 02:01:02 +0200

    Installing Mocha + writting tests desc (not implemented)

---
+++ test/unitTests.js
@@ -0,0 +1,83 @@
+var assert = require('assert');
+
+describe('Initialization', function() {
+  it('default controls are hidden', function() {
+  });
+
+  it('media is defined', function(){
+
+  });
+});
+
+describe('Video Controls', function() {
+	it('video is playing when play btn is clicked',function(){
+
+	});
+
+	it('video is playing when play btn (On screen) is clicked',function(){
+
+	});
+
+	it('video is paused when pause btn is clicked',function(){
+
+	});
+
+	it('video is paused when pause btn (On screen) is clicked',function(){
+
+	});
+
+	it('video is playing from beginning when replay btn is clicked',function(){
+
+	});
+
+	it('video is stopped when stop btn is clicked',function(){
+
+	});
+
+	it('volume is changed when volume slider is changed',function(){
+
+	});
+
+	it('video is muted when mute btn is clicked',function(){
+
+	});
+
+	it('video is unmuted when UNmute btn is clicked',function(){
+
+	});
+
+	it('video is stopped when it reaches end',function(){
+
+	});
+
+	it('Progress bar is changed when video is playing',function(){
+
+	});
+})
+
+describe('Video Style', function() {
+	it('Play button on screen is visible when video is paused',function(){
+
+	});
+
+	it('Pause button on screen is visible when video is paused',function(){
+
+	});
+
+	it('Play btn  in controls is visible when video is paused',function(){
+
+	});
+
+	it('Pause btn in controls is visible when video is paused',function(){
+
+	});
+
+	it('Mute btn is visible when video is unmuted',function(){
+
+	});
+
+	it('Unmute btn is visible when video is muted',function(){
+
+	});
+
+})
